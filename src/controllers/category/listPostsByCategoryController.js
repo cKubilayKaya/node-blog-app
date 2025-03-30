265c1987a4f18cb01a50a6e73b7beb4f1ba620c5
cKubilayKaya <101182731+cKubilayKaya@users.noreply.github.com>
2025-03-31 01:38:22 +0300

    fix

--- src/controllers/category/listPostsByCategoryController.js
+++ src/controllers/category/listPostsByCategoryController.js
@@ -1,11 +1,18 @@
 import { listPostsByCategoryService } from "../../services/category/listPostsByCategoryService.js";
+import { listPostsWithIsLikedByCategoryService } from "../../services/category/listPostsWithIsLikedByCategoryService.js";
 
 export const listPostsByCategoryController = async (req, res, next) => {
+  const token = req?.header("Authorization")?.includes(" ") ? req?.header("Authorization")?.split(" ")[1] : req?.header("Authorization");
   const { slug } = req.params;
   const { page = 1, limit = 10, order = "asc" } = req.query;
 
   try {
-    const response = await listPostsByCategoryService(Number(page), Number(limit), order, slug);
+    let response;
+    if (token) {
+      response = await listPostsWithIsLikedByCategoryService(Number(page), Number(limit), order, slug, token);
+    } else {
+      response = await listPostsByCategoryService(Number(page), Number(limit), order, slug);
+    }
     res.status(200).json(response);
   } catch (error) {
     next(error);
---
+++ src/services/category/listPostsWithIsLikedByCategoryService.js
@@ -0,0 +1,90 @@
+import { CustomError } from "../../utils/customError.js";
+import prisma from "../../utils/prisma.js";
+import jwt from "jsonwebtoken";
+
+export const listPostsWithIsLikedByCategoryService = async (page, limit, order, slug, token) => {
+  const decodedUser = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  if (!decodedUser?.id) throw new CustomError("Invalid token!", 400);
+
+  if (page <= 0 || limit <= 0) {
+    throw new CustomError("Page and limit must be greater than 0", 400);
+  }
+
+  const validOrder = ["asc", "desc"];
+  if (!validOrder.includes(order)) {
+    throw new CustomError("Order must be 'asc' or 'desc'", 400);
+  }
+
+  const skip = (page - 1) * limit;
+
+  const category = await prisma.category.findUnique({
+    where: { slug },
+    include: {
+      _count: { select: { posts: true } },
+    },
+  });
+
+  if (!category) {
+    throw new CustomError("Category not found", 404);
+  }
+
+  // Get the liked posts of the user
+  const userLikes = await prisma.like.findMany({
+    where: {
+      userId: decodedUser.id,
+    },
+    select: {
+      postId: true,
+    },
+  });
+
+  const likedPostIds = userLikes.map((like) => like.postId);
+
+  // Find the posts in the category
+  const posts = await prisma.post.findMany({
+    where: { categories: { some: { id: category.id } } },
+    skip: skip,
+    take: limit,
+    orderBy: {
+      createdAt: order === "desc" ? "desc" : "asc",
+    },
+    include: {
+      author: {
+        select: {
+          email: true,
+          fullname: true,
+          username: true,
+        },
+      },
+      _count: {
+        select: { comments: true },
+      },
+    },
+  });
+
+  // Add the 'isLiked' property to each post
+  const postsWithLikes = posts.map((post) => {
+    const isLiked = likedPostIds.includes(post.id);
+    return {
+      ...post,
+      isLiked,
+    };
+  });
+
+  const totalPosts = category._count.posts;
+  const totalPages = Math.ceil(totalPosts / limit);
+
+  return {
+    success: true,
+    category: {
+      ...category,
+      posts: postsWithLikes,
+    },
+    pagination: {
+      currentPage: page,
+      totalPages: totalPages,
+      totalPosts: totalPosts,
+      limit: limit,
+    },
+  };
+};
